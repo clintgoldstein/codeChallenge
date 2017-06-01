85dbc9170b835188727ecbff1246bf2641e61385
clint goldstein <clintongoldstein@gmail.com>
2017-05-31 18:33:41 -0700

    Initial Commit

---
+++ code_challange/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  code_challange
+//
+//  Created by clinton goldstein on 5/31/17.
+//  Copyright © 2017 clinton goldstein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
